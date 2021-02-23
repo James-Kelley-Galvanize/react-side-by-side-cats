b50cea2d56ab514337b9f8985e9389727803f18e
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-02-23 13:55:56 -0700

    Refactor to Grid, polish presentation of css files

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # REACT CLASSICAL vs. REACT-HOOKS SIDE-BY-SIDE STARTER
 
-## INSTRUCTIONS
+## Instructions
+
+This app is designed to display _TWO_ React apps side-by-side. 
+
+You've been given two applications that are, from the user's perspective, identical when they render on the page. However, under the hood, they're a bit different! 
+
+You've been provided with the code for a Classical React application, and the scaffolding to implement identical functionality using React Hooks. 
+
+These apps render side-by-side in the browser. Your goal is to add code so that the left side (Classical React) identically matches the right side (React Hooks). 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./ParentApp";
+import "./Dont_Edit_These_Styles.css"; // Don't edit these styles, they're for the side-by-side setup
+import "./Shared_Styles.css"; // This css file is shared by both apps - YOUR STYLES GO HERE
+import ClassApp from "./ClassAppSrc/ClassApp";
+import HooksApp from "./HooksAppSrc/HooksApp";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    {/* <div className="parent-app">
+      <div className="parent-title-bar">
+        <h1 className="parent-column parent-title-left">CLASS</h1>
+        <h1 className="parent-column parent-title-right">HOOKS</h1>
+      </div>
+      <div className="parent-column parent-left-side">
+        <ClassApp />
+      </div>
+      <div className="parent-column parent-right-side">
+        <HooksApp />
+      </div>
+    </div> */}
+    <div id="container">
+      <h2 id="class-header">Class App</h2>
+      <div id="class-app">
+        <ClassApp />
+      </div>
+      <h2 id="hooks-header">Hooks App</h2>
+      <div id="hooks-app">
+        <HooksApp />
+      </div>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
