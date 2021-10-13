a94a336cf548e4a4e093f7183f21f984b17c8326
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-10-13 08:15:55 -0600

    build solution

--- src/HooksAppSrc/HooksApp.js
+++ src/HooksAppSrc/HooksApp.js
@@ -1,11 +1,42 @@
 /* 
 This file is a stand-in for a Hooks-based app.
 */
+import { useState } from "react";
+import CatBox from "./components/CatBox";
+
+
 
 function App() {
+let [cats, setCats]= useState([]);
+
+async function getCatImage() {
+  let res = await fetch("https://api.thecatapi.com/v1/images/search/");
+  let json = await res.json();
+  console.log(json);
+  let imageUrl = await json[0].url;
+  return imageUrl;
+}
+
+function getCatName() {
+  let faker = require("faker");
+  return faker.name.firstName();
+}
+async function addCat(e){
+  e.preventDefault()
+  let url = await getCatImage()
+  let name = getCatName()
+  setCats([...cats, {url, name}])
+}
+
+
   return (
     <div className="App">
-      <h1>I'm the Hooks App</h1>
+      <div className="app-title flex-column">
+        <div>Classical Catbox</div>
+        <button onClick={(e) => addCat(e)}>Cat</button>
+      </div>
+      {/* <button onClick={(e) => this.addCat(e)}>Cat</button> */}
+      <CatBox cats={cats} />
     </div>
   );
 }
