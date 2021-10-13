0555f3b996c7e93ce3ea02590b50976935bb0d04
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-10-13 08:41:20 -0600

    Make useEffect solution

--- src/HooksAppSrc/HooksApp.js
+++ src/HooksAppSrc/HooksApp.js
@@ -1,38 +1,42 @@
-/* 
-This file is a stand-in for a Hooks-based app.
-*/
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CatBox from "./components/CatBox";
 
-
-
 function App() {
-let [cats, setCats]= useState([]);
+  let [cats, setCats] = useState([]);
+  let [catCount, setCatCount] = useState(0);
 
-async function getCatImage() {
-  let res = await fetch("https://api.thecatapi.com/v1/images/search/");
-  let json = await res.json();
-  console.log(json);
-  let imageUrl = await json[0].url;
-  return imageUrl;
-}
+  async function getCatImage() {
+    let res = await fetch("https://api.thecatapi.com/v1/images/search/");
+    let json = await res.json();
+    console.log(json);
+    let imageUrl = await json[0].url;
+    return imageUrl;
+  }
 
-function getCatName() {
-  let faker = require("faker");
-  return faker.name.firstName();
-}
-async function addCat(e){
-  e.preventDefault()
-  let url = await getCatImage()
-  let name = getCatName()
-  setCats([...cats, {url, name}])
-}
+  function getCatName() {
+    let faker = require("faker");
+    return faker.name.firstName();
+  }
 
+  async function addCat(e) {
+    e.preventDefault();
+    setCatCount(catCount + 1);
+  }
+  useEffect(() => {
+    async function makeNewCat(){
+      let url = await getCatImage();
+      let name = getCatName();
+      setCats(current=>[...current, { url, name }]);
+    }
+    if (catCount > cats.length) {
+      makeNewCat()
+    }
+  }, [ catCount]);
 
   return (
     <div className="App">
       <div className="app-title flex-column">
-        <div>Classical Catbox</div>
+        <div>Hooks Catbox</div>
         <button onClick={(e) => addCat(e)}>Cat</button>
       </div>
       {/* <button onClick={(e) => this.addCat(e)}>Cat</button> */}
