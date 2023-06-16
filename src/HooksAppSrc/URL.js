5e923656854305a01431b91dc185bba6acadffcf
James-Kelley-Galvanize <james.kelley@galvanize.com>
2023-06-16 09:14:39 -0600

    remove URL file

--- src/HooksAppSrc/URL.js
+++
@@ -1,8 +0,0 @@
-const urls = {
-  development: `https://localhost/cats`,
-  production: `https://api.thecatapi.com/v1/images/search/`,
-};
-
-const url = urls[process.env.NODE_ENV || "development"];
-
-export default url;
